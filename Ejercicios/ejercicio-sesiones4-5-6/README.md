6138e2f95821c85bda337ab9c1f5113acf7fa814
Yf3rs0n <yferson5020@gmail.com>
2023-05-08 00:00:59 -0500

    commit 1

---
+++ Ejercicios/ejercicio-sesiones4-5-6/README.md
@@ -0,0 +1,13 @@
+## Spring Boot
+Ejercicio 1
+
+Se crea un proyecto en Spring Boot con las dependencias:
+
+* H2
+* Spring Data JPA
+* Spring Web
+* Spring Boot dev tools
+
+Se crea una clase HelloController que sea un controlador REST.
+Dentro de la clase crear un método que retorne un saludo.
+Probar que retorna el saludo desde el navegador y desde Postman.
---
+++ Ejercicios/ejercicio-sesiones4-5-6/src/main/java/com/example/ejerciciosesiones456/EjercicioSesiones456Application.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciosesiones456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioSesiones456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioSesiones456Application.class, args);
+	}
+
+}
